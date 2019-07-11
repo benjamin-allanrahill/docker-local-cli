8664f81c923944972e4a6bd937a9b00fb8a2ec23
allanrab <benjamin.allan-rahill@bms.com>
2019-07-11 08:59:12 -0400

    added startContainter()

--- container_setup.py
+++ container_setup.py
@@ -3,4 +3,25 @@
 # Low level module to run configurations in docker container 
 # Benjamin Allan-Rahill
 
-import subprocess, os.path, 
+import colors, subprocess, os.path
+
+def startContainter(image):
+    docker_start_container =    f"docker run"  \
+                                "--cap -add = SYS_ADMIN" \
+                                "--device=/dev/fuse" \
+                                "--security-opt=apparmor:unconfined" \
+                                "--cap-add=DAC_READ_SEARCH" \
+                                "-d -p 2222:22 -p 8787:8787" \
+                                "{image}"
+
+    _evalOrDie(docker_start_container)
+
+
+
+def _evalOrDie(cmd, msg="ERROR"):
+    try:
+        res = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
+        return res
+    except subprocess.CalledProcessError as cmd_exp:
+        print(msg)
+        print(colors.color(cmd_exp.output, fg='red'))
