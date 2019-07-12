a288fcc53a8681d98a0b32d47f205f5d7da69cce
allanrab <benjamin.allan-rahill@bms.com>
2019-07-12 14:49:45 -0400

    added defaults and tests

--- container_setup.py
+++ container_setup.py
@@ -3,23 +3,25 @@
 # Low level module to run configurations in docker container 
 # Benjamin Allan-Rahill
 
-import subprocess, os.path, sys
+import subprocess, os.path, sys, socket
 from colors import color
-from eval import evalOrDie, yes_or_no
+from eval import evalOrDie, yes_or_no, callWithPipe
 from docker_container import Container
 
-def createAndRun(image):
+def createAndRun(image, r_port='8787'):
     # check to see if the image is local
     if testImagePresence(image):
-        container = Container(image)
+        container = Container(image, r_port=r_port)
         
         # test to see if they already have one running
-        if container.isRunning():
+        if container.isImageRunning():
             print(f"You already have a container running with the image [{color(container.image, fg='blue')}]")
-            pass
-        print("No running containers of this image found. \nStarting new container")
-        container.startContainer()
-        print(f"Your container is now running with ID: {container.cid}")
+            return
+        else:
+            print("No running containers of this image found. \nStarting new container")
+            container.startContainer()
+            print(f"Your container is now running with ID: {container.cid}")
+            print(f"Access {color("RStudio", fg='blue')} at {socket.gethostbyname(socket.gethostname())}:{r_port}")
     else:
         pullImage(image)
         createAndRun(image)
@@ -32,7 +34,8 @@ def testContainer(image):
     print(f"Is the container running?  {c.isRunning()}")
     print("exec in!")
     c.execute('echo HELLO')
-    c.cp('eval.py', '/tmp/eval.py')
+    c.cpTo('eval.py', '/tmp/eval.py')
+    print("Copied!")
     print("Going to remove container")
     c.remove()
 
@@ -51,10 +54,9 @@ def testImagePresence(image_name):
     image = f"{registry}:{port}/{repo}"
     print(f"REPO: {repo}")
     print(f"TAG: {tag}")
-    find_tag_cmd = f"docker images | awk '/$2=={tag}$/ {{f  = 1}}; END {{ exit !f }}'"
+    find_tag_cmd = f"docker image ls | awk '$2==\"{tag}\" {{f  = 1}}; END {{ exit !f }}'"
 
-
-    res, code = evalOrDie(find_tag_cmd, ignore=True)
+    res, code = callWithPipe(find_tag_cmd, ignore=True)
     print(res, code)
     res = res.split()
     if code == 0:
@@ -77,11 +79,12 @@ def findSimilarImages(image):
     grep_cmd = f"docker images | grep {image} | awk '{{print $1; print $2; print $3}}'"
 
     # have to 'ignore' error codes when using grep so the program doesnt quit when no match is made
-    res, code = evalOrDie(grep_cmd, "There was an error trying to find similar images")
+    res, code = callWithPipe(grep_cmd, "There was an error trying to find similar images")
 
     if res != '' and code == 0:
         info = res.replace('\n', ' ').split()
-        print("Found these images under the same repository:" + * (f"\t{info[i*3]}" for i in range(len(info) // 3)))
+        print("Found these images under the same repository:")
+        print(*(f"\t{info[i*3]}" for i in range(len(info) // 3)))
         answ = yes_or_no("would you like to run one of these? (y/n)")
         if answ:
             chooseImage()
@@ -94,8 +97,7 @@ def chooseImage(images):
             'type': 'list',
             'name': 'image',
             'message': 'Which image do you want to run?',
-            Separator('= IMAGES ='),
-            'choices' = [images[i] for i in range(len(images))]
+            'choices': [images[i] for i in range(len(images))]
         }
     ]
     answers = prompt(questions)
