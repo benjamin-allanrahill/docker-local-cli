f64e20b277e5b4eca63ff8e2e354e70459b808f1
allanrab <benjamin.allan-rahill@bms.com>
2019-07-18 14:00:01 -0400

    added cleanup

---
+++ reinvented/cleanup.py
@@ -0,0 +1,12 @@
+# cleanup.py
+
+def cleanup(containers, quiet):
+    if quiet:
+        for c in containers:
+            c.stop()
+            c.remove()
+    else:
+        for c in containers:
+            print(f"Removing container: {c.id}")
+            c.stop()
+            c.remove()
