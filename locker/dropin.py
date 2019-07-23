176da58f4cd5431b7975fc8862c0931a9f852feb
allanrab <benjamin.allan-rahill@bms.com>
2019-07-23 15:31:56 -0400

    added ssh in and exec

--- locker/dropin.py
+++ locker/dropin.py
@@ -1,6 +1,10 @@
 from locker.ssh import sshExec, ssh
+from locker.utils import execute
 
 def dropIn(container, cmd, mode):
+    execute(container, cmd, mode)
+
+def sshIn(container, cmd, mode):
     ports = container.ports
 
     print(ports)
