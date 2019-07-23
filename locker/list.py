ca7b9e5669856047646e8f95a9a0afe2f57295e4
allanrab <benjamin.allan-rahill@bms.com>
2019-07-23 17:18:53 -0400

    added registry format

--- locker/list.py
+++ locker/list.py
@@ -4,10 +4,11 @@ def ps(all):
     
     print(evalOrDie(docker_ps_cmd, "There was an error getting the containers")[0])
 
-def listRegistry():
-    curl_cmd = 'curl -X GET https://docker.rdcloud.bms.com:443/v2/_catalog'
+def listRegistry(registry):
+    curl_cmd = f'curl -X GET https://{registy}/v2/_catalog'
 
     data = evalOrDie(curl_cmd, "There was an error getting the images")
+    print(data)
 
 def listImages():
     image_cmd = 'docker images'
