af18eaec32702ff4bd19b2137693328d3d45f782
allanrab <benjamin.allan-rahill@bms.com>
2019-07-23 17:15:16 -0400

    added grap and add

--- locker/files.py
+++ locker/files.py
@@ -1,10 +1,12 @@
 # files.py
 
-from locker.ssh import sshExec
+from locker.utils import cpFrom, cpTo
 
 def add(source, dest, container):
     print(f"Copying file {source} to {dest} in the container {container.id}")
-    sshExec()
+    cpTo(container, source, dest)
+    
 
 def grab(source, dest, container):
     print(f"Copying file {source} in the container {container.id} to {dest}")
+    cpFrom(container, source, dest)
