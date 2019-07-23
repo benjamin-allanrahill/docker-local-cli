95c4279619919694e380ba7479da1530d8727d92
allanrab <benjamin.allan-rahill@bms.com>
2019-07-23 10:20:42 -0400

    initial commit

---
+++ locker/files.py
@@ -0,0 +1,10 @@
+# files.py
+
+from locker.ssh import sshExec
+
+def add(source, dest, container):
+    print(f"Copying file {source} to {dest} in the container {container.id}")
+    sshExec()
+
+def grab(source, dest, container):
+    print(f"Copying file {source} in the container {container.id} to {dest}")
