64ad63870e159839fcca8d83c61602cd29356731
allanrab <benjamin.allan-rahill@bms.com>
2019-07-24 12:58:17 -0400

    search docker hub

--- locker/list.py
+++ locker/list.py
@@ -12,14 +12,20 @@ def listRegistry(registry):
     #print(data)
     #print(type(data))
 
-    print("REPOSITORY \t\t\t\t\t TAGS")
+    print("\nREPOSITORY \t\t\t\t\t TAGS")
     for repo in data['repositories']:
         tag_cmd = f'curl  -s -X GET https://{registry}/v2/{repo}/tags/list'
         res = json.loads(evalOrDie(tag_cmd, "There was en error getting the tags")[0])
         tags = [str(tag) for tag in res['tags']]
         print(f"{repo} \t\t\t\t\t {tags}")
 
+    print(f"\nUse {registry}/<repo>:<tag> as your environment in the `locker run` cmd.\n")
+    print(f"EXAMPLE:\n\t`locker run --env {registry}/{repo}:{tags[0]}")
+
 def listImages():
     image_cmd = 'docker images'
-    
     print(evalOrDie(image_cmd)[0])
+
+def searchDockerHub(name, limit='15'):
+    search_cmd = f'docker search {name} --limit {limit}'
+    print(evalOrDie(search_cmd)[0])
