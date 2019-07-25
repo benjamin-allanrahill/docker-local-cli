9330d7ab2a78536a3b4d5efdd70b4be40309768b
allanrab <benjamin.allan-rahill@bms.com>
2019-07-25 09:22:05 -0400

    tst code

--- locker/utils.py
+++ locker/utils.py
@@ -2,30 +2,22 @@ import pexpect
 from subprocess import call
 from pexpect.popen_spawn import PopenSpawn
 from locker.eval import evalOrDie
+
+
 def cpTo(container, file_path, dest):
-    docker_cp_cmd = (
-                    "docker cp "
-                    f"{file_path} "
-                    f"{container.id[:3]}:{dest}"
-    )
+    docker_cp_cmd = ("docker cp " f"{file_path} " f"{container.id[:3]}:{dest}")
     evalOrDie(docker_cp_cmd)
+
 
 def cpFrom(container, file_path, dest):
-    docker_cp_cmd = (
-                    "docker cp "
-                    f"{container.id[:3]}:{file_path} "
-                    f"{dest}"
-    )
+    docker_cp_cmd = ("docker cp " f"{container.id[:3]}:{file_path} " f"{dest}")
     evalOrDie(docker_cp_cmd)
 
+
 def execute(container, cmd, flags=''):
-    docker_exec_cmd = (
-                        "docker exec "
-                        f"-{flags} "
-                        f"{container.id[:3]} "
-                        f"{cmd}"
-    )
+    print(f"EXECUTING: {cmd}")
+    docker_exec_cmd = ("docker exec "
+                       f"-{flags} "
+                       f"{container.id[:3]} "
+                       f"{cmd}")
     call(docker_exec_cmd)
-    
-
-
