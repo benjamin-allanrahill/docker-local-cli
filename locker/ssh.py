5ef4994bbba1592d2391ed6128a990856d2abbbd
allanrab <benjamin.allan-rahill@bms.com>
2019-07-25 12:22:30 -0400

    docstrings

--- locker/ssh.py
+++ locker/ssh.py
@@ -1,9 +1,27 @@
+#!/usr/bin/env python3.6
+"""
+ssh.py
+@description: Functions for Locker ssh subcommand 
+@author: Benjamin Allan-Rahill
+
+"""
 import paramiko, os, cmd, subprocess
 from locker.utils import cpFrom, cpTo, execute
 from colors import color
 
+
 def ssh(dport):
-    ssh_cmd = [ 'ssh', 'domino@localhost', '-p', f'{dport}']
+    '''
+        ssh(dport)
+
+        ssh into a running container   
+            
+        Parameters
+        ----------
+        dport: str
+            the port locally that maps to the ssh port in the container
+    '''
+    ssh_cmd = ['ssh', 'domino@localhost', '-p', f'{dport}']
     # subprocess.call(' '.join(ssh_cmd), shell=True)
     try:
         subprocess.call(' '.join(ssh_cmd), shell=True)
@@ -11,55 +29,73 @@ def ssh(dport):
         print("There was an error during ssh")
         exit(1)
 
+
 def copyKeys(container, location, user):
+    '''
+        copyKeys(container, location, user)
+        
+        copy ssh keys into a running container 
+            
+        Parameters
+        ----------
+        container: Container
+            the container to copy keys to 
+        location: str
+            path to the .ssh directory 
+        user: str
+            the name of the user 
+
+    '''
 
     wkdir = os.getcwd()
 
     # test to see if location exists
     if not os.path.exists(location):
-        location = input(f"I was looking for your ssh keys at {color(location, fg='red')}. Where should I look?")
+        location = input(
+            f"I was looking for your ssh keys at {color(location, fg='red')}. Where should I look?"
+        )
     #change wkdir do that keys can be copied
     os.chdir(location)
 
     cpTo(container, f"id_rsa", f"/home/domino/.ssh/id_rsa_{user}")
     cpTo(container, f"id_rsa.pub", f"/home/domino/.ssh/id_rsa_{user}.pub")
-   
-    # append public key to authorized keys 
-    execute(container, f"cat /home/domino/.ssh/id_rsa_{user}.pub >> /home/domino/.ssh/authorized_keys")
+
+    # append public key to authorized keys
+    execute(
+        container,
+        f"cat /home/domino/.ssh/id_rsa_{user}.pub >> /home/domino/.ssh/authorized_keys"
+    )
 
     os.chdir(wkdir)
 
+
 def sshExec(cmd, sport):
+    '''
+        sshExec(cmd, sport)
+
+        run a command detached through ssh        
+            
+        Parameters
+        ----------
+        cmd: str
+            the shell command to run through ssh 
+        sport: str
+            the port locally that maps to the ssh port in the container
+
+    '''
+
     client = paramiko.SSHClient()
-    
+
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
     print(f"connecting...")
-    client.connect(hostname='localhost', port=sport, username='domino', password='domino')
+    client.connect(hostname='localhost',
+                   port=sport,
+                   username='domino',
+                   password='domino')
     print("connected")
 
     print(f"Running {color(cmd, fg='yellow')}")
     stdin, stdout, stderr = client.exec_command(cmd)
     print(stdout)
     print('Executed command')
-
-
-class SshCommand(cmd.Cmd):
-    """ Simple shell to run a command on the host """
-
-    prompt = 'locker ssh @localhost >>> '
-
-    def __init__(self, host):
-        cmd.Cmd.__init__(self)
-        self.host = host
-
-    def do_run(self, command):
-        """run 
-        Execute this command on all hosts in the list"""
-        if command:
-            stdin, stdout, stderr = self.host.exec_command(command)
-            stdin.close()
-            for line in stdout.read().splitlines():
-                print( 'host: %s: %s' % (self.host, line))
-        else:
-            print("usage: run ")
